c86d26a50d503df59dca5068412ed26254d61816
ibrahim925 <ikb1703@gmail.com>
2024-08-05 14:10:23 -0400

    first commit

--- pocket-prosthetic-backend/build/common/constants/fields.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fields = void 0;
-exports.fields = {
-    USER_EMAIL: "USER_EMAIL",
-    USER_PASSWORD: "USER_PASSWORD",
-};
-//# sourceMappingURL=fields.js.map
